61a5f0fb22cbe6264609663f139b7f5e59e5ac88
komissarowvm-byte <komissarowvm@gmail.com>
2026-01-17 13:57:44 +0800

    Update KR2_3_2_komissarov.cpp

--- KR_2/KR2_3_2_komissarov.cpp
+++ KR_2/KR2_3_2_komissarov.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void printReverseNegated() {
+    int num;
+    if (cin >> num) {
+        printReverseNegated();
+        cout << -num << " ";
+    }
+}
+
+int main() {
+    cout << "Введите числа через пробел (для завершения введите любой нечисловой символ):" << endl;
+    printReverseNegated();
+    cout << endl;
+    
+    return 0;
+}
 
